f6d25cac8cb484ad32cc5c6c32f9eb2c998e94ae
Ademola <adigun673@gmail.com>
2020-02-17 21:27:52 +0100

    added Frontend

---
+++ FarmCLIENT/Cabbage.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <?php
+    require_once 'vendor/autoload.php';
+ 
+    use JsonSchema\Validator;
+    use JsonSchema\Constraints\Constraint;
+
+    $config = json_decode(file_get_contents('http://localhost:8080/cabbage.json'));
+    $validator = new Validator; 
+    $validator->validate(
+    $config,
+    (object)['$ref' => 'file://' . realpath('js/schema.json')],
+    Constraint::CHECK_MODE_APPLY_DEFAULTS
+    );
+
+    if ($validator->isValid()) {
+     echo "<h1>JSON</h1>";
+     echo "<a href='Sunflower.php'> Sunflower <a><br>";
+     echo "<a href='Potato.php'> Potato <a>";
+     echo "<script>console.log('JSON Validated!')</script>";
+ ?>
+<script>
+window.onload = function() {
+
+var dataPoints = [];
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Cabbage patch Temperatures"
+	},
+	axisY: {
+		title: "Degrees Celcius",
+		titleFontSize: 24
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,### Units",
+		dataPoints: dataPoints
+	}]
+});
+
+function addData(data) {
+	for (var i = 0; i < data.length; i++) {
+		dataPoints.push({
+			x: new Date(data[i].date),
+			y: data[i].temperature
+		});
+	}
+	chart.render();
+
+}
+
+$.getJSON("http://localhost:8080/cabbage.json", addData);
+
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<a href="CabbageXML.php"><p>Switch to XML</p></a>
+</body>
+<?php
+    }
+?>
+</html>
---
+++ FarmCLIENT/CabbageXML.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<?php
+	
+//Validator
+
+include "xsd/validator.php";
+function libxml_display_errors() {
+	$errors = libxml_get_errors();
+	foreach ($errors as $error) {
+		print libxml_display_error($error);
+	}
+	libxml_clear_errors();
+}
+
+// Enable user error handling
+
+libxml_use_internal_errors(true);
+
+$xml = new DOMDocument();
+$xml->load('http://localhost:8080/cabbage.xml');
+
+if (!$xml->schemaValidate('xsd/schema.xsd')) {
+	print '<b>DOMDocument::schemaValidate() Generated Errors!</b>';
+	libxml_display_errors();
+} else {
+	echo "<script>console.log('XML Validated!')</script>";
+
+ ?>
+<script>
+window.onload = function() {
+
+var dataPoints = [];
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Cabbage patch Temperatures"
+	},
+	axisY: {
+		title: "Degrees Celcius",
+		titleFontSize: 24
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,### Units",
+		dataPoints: dataPoints
+	}]
+});
+
+function addData(data) {
+	$(data).find("item").each(function () {
+		var $dataPoint = $(this);
+		var x = $dataPoint.find("date").text();
+		var y = $dataPoint.find("temperature").text();
+		dataPoints.push({x: parseFloat(x), y: parseFloat(y)});
+	});
+
+}
+
+$.get("http://localhost:8080/cabbage.xml", addData);
+
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<a href="Cabbage.php"><p>Switch to JSON</p></a>
+</body>
+<?php
+}
+?>
+</html>
